e2c0e77c4b9a0835edb78efdf28ff34369675906
Ayo <ramon.aycojr@gmail.com>
2023-05-11 16:19:57 +0200

    feat: accept URL from form or searchParam

---
+++ src/utils/get-params.ts
@@ -0,0 +1,16 @@
+export default function getParams(
+  url: URL
+): { [key: string]: string } | undefined {
+  if (url.search === "") return;
+
+  return Object.fromEntries(
+    url.search
+      .replace("?", "")
+      .split("&")
+      .map((paramstr) => paramstr.split("="))
+      .map(([key, value]) => [
+        key,
+        decodeURIComponent(value?.replace(/\+/g, "%20")),
+      ])
+  );
+}
