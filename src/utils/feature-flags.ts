ad62e9ee0ccc1322b57de0552d431eefa9eb6be7
Ayo <ramon.aycojr@gmail.com>
2023-07-27 18:40:54 +0200

    feat: disable hide-images feature

--- src/utils/feature-flags.ts
+++ src/utils/feature-flags.ts
@@ -2,5 +2,5 @@ export type Features = 'Send to Email' | 'Hide Images';
 
 export const featureFlags: Record<Features, boolean> = {
   'Send to Email': false,
-  'Hide Images': true,
+  'Hide Images': false,
 };
