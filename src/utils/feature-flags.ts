2fe2708cc977ee8334f21bd54513e3488929c662
Ayo <ramon.aycojr@gmail.com>
2023-07-28 10:02:13 +0200

    refactor: Feature type

--- src/utils/feature-flags.ts
+++ src/utils/feature-flags.ts
@@ -1,6 +1,6 @@
-export type Features = 'Send to Email' | 'Hide Images';
+export type Feature = 'Send to Email' | 'Hide Images';
 
-export const featureFlags: Record<Features, boolean> = {
+export const featureFlags: Record<Feature, boolean> = {
   'Send to Email': false,
   'Hide Images': false,
 };
