af47310a937aeaf4a27d48d93f4c55fe29fbb089
ayoayco <ayo@ayco.io>
2024-07-27 07:57:42 +0200

    feat: switch from vercel -> node SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/serverless';
-import prefetch from "@astrojs/prefetch";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [prefetch()],
-  adapter: vercel()
+  adapter: node({
+    mode: "standalone"
+  })
 });
