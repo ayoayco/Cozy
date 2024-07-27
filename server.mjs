4d0772a9aedcce1d356892f3fd83de19cce01464
ayoayco <ayo@ayco.io>
2024-07-27 11:42:41 +0200

    feat: remove fastify-cli

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import Fastify from 'fastify';
 import fastifyMiddie from '@fastify/middie';
 import fastifyStatic from '@fastify/static';
 import { fileURLToPath } from 'node:url';
-import { handler as ssrHandler } from '../dist/server/entry.mjs';
+import { handler as ssrHandler } from './dist/server/entry.mjs';
 
 const app = Fastify({ logger: true });
 
