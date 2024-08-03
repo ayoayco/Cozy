07ee133ec725a7af425bbfa24069c8e433cc42fb
ayoayco <ayo@ayco.io>
2024-08-03 09:30:16 +0200

    refactor: move service-worker out of utils

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   }),
   integrations: [
     serviceWorker({
-      path: "./src/utils/sw.js",
+      path: "./src/sw.js",
       assetCachePrefix: 'cozy-reader',
     })
   ]
