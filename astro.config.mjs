ad7f30bb175212102eae00cf6a3619e2dfda81c6
ayoayco <ayo@ayco.io>
2024-08-14 14:56:01 +0200

    feat: initial /blog site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import serviceWorker from "@ayco/astro-sw";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "hybrid",
   adapter: node({
     mode: "middleware"
   }),
   integrations: [
+    mdx(),
+    sitemap(),
     serviceWorker({
       path: "./src/sw.js",
       assetCachePrefix: 'cozy-reader',
---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Cozy Blog';
+export const SITE_DESCRIPTION = 'The Web is Yours.';
