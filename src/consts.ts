75fe8b96d22d9a838118f72bd6727b8a6a64fa64
ayoayco <ayo@ayco.io>
2024-08-16 10:30:20 +0200

    feat(blog): a11y & various style improvements; add author

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 export const SITE_TITLE = 'Cozy Blog';
 export const SITE_AUTHOR = 'Ayo Ayco';
+export const SITE_AUTHOR_URL = 'https://ayo.ayco.io';
 export const SITE_AUTHOR_MASTODON = 'https://social.ayco.io/@ayo';
 export const SITE_PROJECT_REPO = 'https://github.com/ayoayco/Cozy';
 export const SITE_DESCRIPTION = 'The Web is Yours.';
