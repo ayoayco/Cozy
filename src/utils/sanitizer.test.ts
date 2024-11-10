d5b373df483fd242ba1a18636d5bf2b3c0c9a9e9
Ayo Ayco <ayo@ayco.io>
2024-11-10 20:05:39 +0100

    test(sanitizer): add tests for cozify()

---
+++ src/utils/sanitizer.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, test } from "vitest";
+import { cozify } from "./sanitizer";
+
+describe("cozify()", async () => {
+  const baseUrl = "https://cozy.pub";
+
+  test("should remove scripts", async () => {
+    const html = "<h1>HELLO</h1><script>console.log()</script>";
+    const result = await cozify(html, baseUrl);
+    expect(result).not.toContain("<script>");
+  });
+
+  test("should remove target=_blank from links", async () => {
+    const html = "<a href=# target='_blank'>hey</a>";
+    const result = await cozify(html, baseUrl);
+    expect(result).not.toContain("target");
+    console.log(result);
+  });
+
+  test("should add base url to href of links", async () => {
+    const html = "<a href=#>hey</a>";
+    const result = await cozify(html, baseUrl);
+    expect(result).toContain('href="https://cozy.pub?url=#"');
+  });
+
+  test("should add prefetch=true of links", async () => {
+    const html = "<a href=#>hey</a>";
+    const result = await cozify(html, baseUrl);
+    expect(result).toContain('prefetch="true"');
+  });
+});
