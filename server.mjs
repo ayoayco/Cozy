02cd6f4b5f44d66bbfdca1ba18846d9a440b2a46
Ayo <ayo@ayco.io>
2025-08-07 18:45:14 +0200

    feat: add rate limiter in fastify server

--- server.mjs
+++ server.mjs
@@ -7,6 +7,28 @@ import { fileURLToPath } from 'node:url'
 import { handler as ssrHandler } from './dist/server/entry.mjs'
 
 const app = Fastify({ logger: true })
+await app.register(import('@fastify/rate-limit'), {
+  global: true,
+  max: 25,
+  timeWindow: 1000,
+})
+
+await app.setNotFoundHandler(
+  {
+    preHandler: app.rateLimit(),
+  },
+  function (request, reply) {
+    reply.code(404).send({ nothing: 'to see here' })
+  }
+)
+
+await app.setErrorHandler(function (error, request, reply) {
+  if (error.statusCode === 429) {
+    reply.code(429)
+    error.message = 'You hit the rate limit! Slow down please!'
+  }
+  reply.send(error)
+})
 
 await app
   .register(fastifyStatic, {
